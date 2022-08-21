8ac9c38e24d821d5652d481e555754d4e17cdc7a
NikaKhiz <natiajojua2020@gmail.com>
2022-08-21 19:58:22 +0400

    Created projects search custom block and implemented js

---
+++ web/modules/custom/projects/src/Plugin/Block/ProjectsBlock.php
@@ -0,0 +1,29 @@
+<?php
+namespace Drupal\projects\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+
+
+/**
+ * Provides a 'Ged Currency Chart Block' Block.
+ *
+ * @Block(
+ *   id = "projects_block",
+ *   admin_label = @Translation("Projects Block"),
+ *   category = @Translation("Projects"),
+ * )
+ */
+
+class ProjectsBlock extends BlockBase {
+  public function build() {
+
+    return [
+        // '#markup' => $this->t('Hello, World!'),
+        '#theme' => 'projects',
+        '#attached' => [
+            'library' => [
+              'projects/projects_block_lib',
+            ],
+        ],
+    ];
+  }
+}

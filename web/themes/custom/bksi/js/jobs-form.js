db24b3f734f3591a71c04a29642b1c3e2cc30af0
NikaKhiz <natiajojua2020@gmail.com>
2022-09-29 05:11:15 +0400

    "#195 - modifie job apply form install inline form module Modifie necessarry lines  in contact form custom module also add js file for testing purposes and according to that modifie bksi libraries and theme files add several lines to tailwind custom css file to style form inline errors"

---
+++ web/themes/custom/bksi/js/jobs-form.js
@@ -0,0 +1,12 @@
+const inputElement = document.querySelector('.form-file');
+
+inputElement.addEventListener("change", handleFiles);
+function handleFiles() {
+    console.log(
+        document.querySelector('.file-cont')
+    )
+}
+
+
+
+

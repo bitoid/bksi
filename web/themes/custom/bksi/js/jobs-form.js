88cc9a70f2f8dc5f36bdc9f7b4b26e89afaf1b00
anemoiaa <georgiypetroff@gmail.com>
2022-10-12 00:26:08 +0400

    #195 implement remove file via click on checkbox

--- web/themes/custom/bksi/js/jobs-form.js
+++ web/themes/custom/bksi/js/jobs-form.js
@@ -1,12 +1,22 @@
-const inputElement = document.querySelector('.form-file');
+(function ($, Drupal) {
+    Drupal.behaviors.fileAutoUpload = {
+        attach: function attach(context) {
+            $(once('auto-file-upload', 'input[type="file"]', context)).on('change.autoFileUpload', function(e) {
+                Drupal.file.triggerUploadButton(e);
+                const fileUploadSection = document.querySelector("#file-upload-section");
+                fileUploadSection.addEventListener('click', (e) => {
+                    if(e.target.type === 'checkbox' && e.target.id.startsWith("edit-file-file-")){
+                        const fileRemoveButton = document.querySelector('[name="file_remove_button"]');
+                        fileRemoveButton.dispatchEvent(new Event('mousedown')); 
+                    }
+                });
 
-inputElement.addEventListener("change", handleFiles);
-function handleFiles() {
-    console.log(
-        document.querySelector('.file-cont')
-    )
-}
-
-
-
-
+            });
+        },
+        detach: function detach(context, settings, trigger) {
+            if (trigger === 'unload') {
+                $(once.remove('auto-file-upload', 'input[type="file"]', context)).off('.autoFileUpload');
+            }
+        }
+    };
+})(jQuery, Drupal);

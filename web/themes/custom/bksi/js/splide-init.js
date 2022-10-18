213ebf2455eef306c8007ae22acb83af8ec41800
Jemo <sepishvili02@gmail.com>
2022-10-18 18:51:30 +0400

    #231 caption on career page

--- web/themes/custom/bksi/js/splide-init.js
+++ web/themes/custom/bksi/js/splide-init.js
@@ -1,23 +1,28 @@
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
     new Splide(
         '.splide', 
         {
-            type   : 'slide',
-            rewind: true,
+            type : 'slide',
+            rewind:true,
             fixedWidth: '50%',
-            perPage: 3,
-            focus: "center",
+            perPage: 1,
+            focus:"center",
             gap: 40,
-            start: 1,
+            start: 0,
             trimSpace: false,
+            drag:false,
             breakpoints: {
-                768: {
+                768: {                 
                     perPage: 1,
                     gap: 0,
-                    fixedWidth: '100%',
+                    fixedWidth: '100%',                   
                 },
             },
             arrows: 2,
             pagination: 0,
         }).mount()
 });
+
+

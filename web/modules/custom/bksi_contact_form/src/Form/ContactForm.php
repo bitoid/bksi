66877682a7ccfbdebd7e74901480d32e0cadce54
NikaKhiz <khizambarelinika123@gmail.com>
2022-10-19 14:38:38 +0400

    #195 - Modifie contact form submit button rendering logic

--- web/modules/custom/bksi_contact_form/src/Form/ContactForm.php
+++ web/modules/custom/bksi_contact_form/src/Form/ContactForm.php
@@ -6,170 +6,175 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 
-class ContactForm extends FormBase {
+class ContactForm extends FormBase
+{
 
-    /**
-     * Getter method for Form ID.
-     *
-     * @return string
-     *   The unique ID of the form defined by this class.
-     */
-    public function getFormId() {
-      return 'bksi_contact_form';
-    }
-
-
-    /**
-     *
-     * @param array $form
-     *   Default form array structure.
-     * @param \Drupal\Core\Form\FormStateInterface $form_state
-     *   Object containing current form state.
-     *
-     * @return array
-     *   The render array defining the elements of the form.
-    */
-    public function buildForm(array $form, FormStateInterface $form_state) {
-      
-      $form['#attributes'] = array('enctype' => 'multipart/form-data');
-      $form['#cache']['max-age'] = 300;
-      
-      $form['name'] = [
-        '#type' => 'textfield',
-        '#attributes' => ['placeholder' => t('Name')],
-        '#required' => TRUE,
-      ];
-
-      $form['surname'] = [
-        '#type' => 'textfield',
-        '#attributes' => ['placeholder' => t('Nachname')],
-        '#required' => TRUE,
-      ];
-
-      $form['email'] = [
-        '#type' => 'email',
-        '#attributes' => ['placeholder' => t('E-mail')],
-        '#required' => TRUE,
-      ];      
-
-      $form['phone'] = [
-        '#type' => 'tel',
-        '#attributes' => ['placeholder' => t('Telefon')],
-        '#required' => TRUE,
-      ];
-
-      $form['message'] = [
-        '#type' => 'textarea',
-        '#attributes' => ['placeholder' => t('Nachricht')],
-        '#required' => TRUE,
-      ];
-
-      $form['file'] = [
-        '#type' => 'managed_file',
-        '#required' => TRUE,
-        '#multiple' => TRUE,
-        '#upload_validators' => [
-          'file_validate_extensions' => array('pdf'),
-          'file_validate_size' => array(5000000)
-        ],  
-        '#upload_location' => 'public://form_files/'
-      ];
-
-      $form['policy_checkbox'] = [
-        '#type' => 'checkbox',
-        '#required' => TRUE,
-      ];
+  /**
+   * Getter method for Form ID.
+   *
+   * @return string
+   *   The unique ID of the form defined by this class.
+   */
+  public function getFormId()
+  {
+    return 'bksi_contact_form';
+  }
 
 
-      // Add a submit button that handles the submission of the form.
+  /**
+   *
+   * @param array $form
+   *   Default form array structure.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Object containing current form state.
+   *
+   * @return array
+   *   The render array defining the elements of the form.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+
+    $form['#attributes'] = array('enctype' => 'multipart/form-data');
+    $form['#cache']['max-age'] = 300;
+
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#attributes' => ['placeholder' => t('Name')],
+      '#required' => TRUE,
+    ];
+
+    $form['surname'] = [
+      '#type' => 'textfield',
+      '#attributes' => ['placeholder' => t('Nachname')],
+      '#required' => TRUE,
+    ];
+
+    $form['email'] = [
+      '#type' => 'email',
+      '#attributes' => ['placeholder' => t('E-mail')],
+      '#required' => TRUE,
+    ];
+
+    $form['phone'] = [
+      '#type' => 'tel',
+      '#attributes' => ['placeholder' => t('Telefon')],
+      '#required' => TRUE,
+    ];
+
+    $form['message'] = [
+      '#type' => 'textarea',
+      '#attributes' => ['placeholder' => t('Nachricht')],
+      '#required' => TRUE,
+    ];
+
+    $form['file'] = [
+      '#type' => 'managed_file',
+      '#required' => false,
+      '#multiple' => TRUE,
+      '#upload_validators' => [
+        'file_validate_extensions' => array('pdf'),
+        'file_validate_size' => array(5000000)
+      ],
+      '#upload_location' => 'public://form_files/'
+    ];
+
+    $form['policy_checkbox'] = [
+      '#type' => 'checkbox',
+      '#required' => TRUE,
+    ];
+
+
+    // Add a submit button that handles the submission of the form if form  isnt already submited.
+    if (empty($this->messenger()->all())) {
       $form['actions']['submit'] = [
         '#type' => 'submit',
         '#value' => $this->t('Submit'),
       ];
-
-      $form['#theme'] = 'contact_form';
-
-      return $form;
     }
 
+    $form['#theme'] = 'contact_form';
 
-    /**
-     * Implements form validation.
-     *
-     * @param array $form
-     *   The render array of the currently built form.
-     * @param \Drupal\Core\Form\FormStateInterface $form_state
-     *   Object describing the current state of the form.
-     */
-    public function validateForm(array &$form, FormStateInterface $form_state) {
-      $values = ($form_state->getUserInput());
-
-      if (strlen($values['name']) < 2) {
-        $form_state->setErrorByName(
-          'name',
-          $this->t('Name field must contain 2 or more characters')
-        );
-      }
-
-      if (strlen($values['surname']) < 2) {
-        $form_state->setErrorByName(
-          'surname',
-          $this->t('Surname field must contain 2 or more characters')
-        );
-      }
-
-      if (!\Drupal::service('email.validator')->isValid($values['email'])) {
-        $form_state->setErrorByName(
-          'email',
-          $this->t('The email address @mail is not valid.', array('@mail' => $values['email']))
-        );
-      }
-
-      if (!$values['policy_checkbox']) {
-        $form_state->setErrorByName(
-          'policy_checkbox',
-          $this->t('To continue, you must agree to the privacy policy')
-        );
-      }
-    }
+    return $form;
+  }
 
 
-    /**
-     * Implements a form submit handler.
-     *
-     * @param array $form
-     *   The render array of the currently built form.
-     * @param \Drupal\Core\Form\FormStateInterface $form_state
-     *   Object describing the current state of the form.
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-      $values = $form_state->getUserInput();
+  /**
+   * Implements form validation.
+   *
+   * @param array $form
+   *   The render array of the currently built form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Object describing the current state of the form.
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
+    $values = ($form_state->getUserInput());
 
-      $fids = explode(" ", $values['file']['fids']);
-      $files = \Drupal\file\Entity\File::loadMultiple($fids);
-      $files_url = array_map(fn($file) => $file->getFileUri(), $files);
-      $params = array(
-        'name' => $values['name'],
-        'surname' => $values['surname'],
-        'email' => $values['email'],
-        'message' => $values['message'],
-        'node_id' => $values['node_id'],
-        'attachments' => array_map
-        (
-          fn($file) => ['filecontent' => file_get_contents($file), 'filename' => 'asdasd.pdf'], $files_url
-        ),
+    if (strlen($values['name']) < 2) {
+      $form_state->setErrorByName(
+        'name',
+        $this->t('Name field must contain 2 or more characters')
       );
-
-      $mailManager = \Drupal::service('plugin.manager.mail');
-      $module = $this->getFormId();
-      $key = 'contact_form';
-      $to = 'test@mailhog.local';
-      $langcode = \Drupal::currentUser()->getPreferredLangcode();
-      $send = TRUE;
-      $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
-
-      $this->messenger()->addStatus($this->t('Vielen Dank für Deine Bewerbung! Wir werden Deine Bewerbung schnellstmöglich bearbeiten und uns melden. Dies kann jedoch auch einmal einige Tage dauern.'));
     }
-    
 
+    if (strlen($values['surname']) < 2) {
+      $form_state->setErrorByName(
+        'surname',
+        $this->t('Surname field must contain 2 or more characters')
+      );
+    }
+
+    if (!\Drupal::service('email.validator')->isValid($values['email'])) {
+      $form_state->setErrorByName(
+        'email',
+        $this->t('The email address @mail is not valid.', array('@mail' => $values['email']))
+      );
+    }
+
+    if (!$values['policy_checkbox']) {
+      $form_state->setErrorByName(
+        'policy_checkbox',
+        $this->t('To continue, you must agree to the privacy policy')
+      );
+    }
+  }
+
+
+  /**
+   * Implements a form submit handler.
+   *
+   * @param array $form
+   *   The render array of the currently built form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Object describing the current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    $values = $form_state->getUserInput();
+
+    $fids = explode(" ", $values['file']['fids']);
+    $files = \Drupal\file\Entity\File::loadMultiple($fids);
+    $files_url = array_map(fn ($file) => $file->getFileUri(), $files);
+    $params = array(
+      'name' => $values['name'],
+      'surname' => $values['surname'],
+      'email' => $values['email'],
+      'message' => $values['message'],
+      'node_id' => $values['node_id'],
+      'attachments' => array_map(
+        fn ($file) => ['filecontent' => file_get_contents($file), 'filename' => 'asdasd.pdf'],
+        $files_url
+      ),
+    );
+
+    $mailManager = \Drupal::service('plugin.manager.mail');
+    $module = $this->getFormId();
+    $key = 'contact_form';
+    $to = 'test@mailhog.local';
+    $langcode = \Drupal::currentUser()->getPreferredLangcode();
+    $send = TRUE;
+    $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
+
+    $this->messenger()->addStatus($this->t('Vielen Dank für Deine Bewerbung! Wir werden Deine Bewerbung schnellstmöglich bearbeiten und uns melden. Dies kann jedoch auch einmal einige Tage dauern.'));
+  }
 }

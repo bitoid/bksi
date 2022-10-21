a7a977c183e5fe9ba4a6b2dcca642bf1fbe601a7
Guga Karalashvili <guga.karalashvili@gmail.com>
2022-10-21 17:51:30 +0400

    #214 Convert images to webp in custom modules twig

--- web/modules/custom/projects/js/script.js
+++ web/modules/custom/projects/js/script.js
@@ -41,7 +41,13 @@ function displayData(arr) {
         <div class="group md:relative md:overflow-hidden">
                <a href="${projectsData['url']}" class="${parseInt(projectsData['tick']) !== 1 ? 'pointer-events-none relative block w-full h-56 md:h-[400px] mb-5 md:mb-0' : 'relative block w-full h-56 md:h-[400px] mb-5 md:mb-0'}">
                     <div class="relative fade-in-image-container h-full">
-                        <img class="w-full fade-in-image h-full object-cover" src="${projectsData['image'] ? projectsData['image'] : ''}" alt=""/>
+                        <picture>
+                          <source srcset="${projectsData['image']['large_webp'] ? projectsData['image']['large_webp'] : ''}" media="only screen and (max-width: 450px)" type="image/webp" >
+                          <source srcset="${projectsData['image']['large_jpg'] ? projectsData['image']['large_jpg'] : ''}" media="only screen and (max-width: 450px)" type="image/jpeg" >
+                          <source srcset="${projectsData['image']['original_webp'] ? projectsData['image']['original_webp'] : ''}" media="only screen and (min-width: 450px)" type="image/webp">
+                          <source srcset="${projectsData['image']['original_jpg'] ? projectsData['image']['original_jpg'] : ''}" media="only screen and (min-width: 450px)" type="image/jpeg">
+                          <img class="w-full fade-in-image h-full object-cover" src="${projectsData['image']['original_jpg'] ? projectsData['image']['original_jpg'] : ''}" alt=""/>
+                        </picture>
                     </div>
                     <span class="absolute right-5 bottom-5 w-10 h-10 rounded-full bg-white flex items-center justify-center md:hidden"><img src="modules/custom/projects/images/arrow-textlinks.svg" alt=""/></span>
                     <h4 class="hidden text-2xl absolute bottom-10 left-10 text-white font-semibold group-hover:bottom-[calc(100%-60px)] group-hover:translate-y-1/2 z-10 md:block">${projectsData['title']}</h4>
